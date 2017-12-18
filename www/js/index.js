5635bb9b9f10e2e380e40a58d73d23d1ee2a5582
Dave Alden <dpa99c@gmail.com>
2017-12-18 09:50:48 +0000

    Test clearing of webview data

--- www/js/index.js
+++ www/js/index.js
@@ -10,17 +10,45 @@ function onDeviceReady(){
 
 function clearCache(){
     console.log("clearCache()");
-    ClearData.cache();
+    ClearData.cache(function(){
+        console.log("Cleared cache data")
+    }, function(error){
+        console.error("Error clearing cache data: "+error)
+    });
 }
 
 function clearWebview(){
     console.log("clearWebview()");
-    ClearData.webview();
+
+    var dataTypes = $('#webview-data').val();
+    if(dataTypes){
+        for(var i=0; i<dataTypes.length; i++){
+            dataTypes[i] = ClearData.WEBVIEW[dataTypes[i]];
+        }
+    }
+
+    ClearData.webview(function(){
+        console.log("Cleared webview data")
+    }, function(error){
+        console.error("Error clearing webview data: "+error)
+    }, dataTypes);
 }
 
 function clearApplication(){
     console.log("clearApplication()");
-    ClearData.application();
+
+    var dataTypes = $('#application-data').val();
+    if(dataTypes){
+        for(var i=0; i<dataTypes.length; i++){
+            dataTypes[i] = ClearData.APPLICATION[dataTypes[i]];
+        }
+    }
+
+    ClearData.application(function(){
+        console.log("Cleared application data")
+    }, function(error){
+        console.error("Error clearing application data: "+error)
+    }, dataTypes);
 }
 
 $(document).on('deviceready', onDeviceReady);
